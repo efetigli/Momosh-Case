2e4c1773bf3f8622010a03c40df6b303287d7c88
EfeTigli <netigli@hotmail.com>
2022-10-24 23:06:20 +0300

    Create CaptureCriminalByPlayer Script

---
+++ Assets/Scripts/Player/CaptureCriminalByPlayer.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaptureCriminalByPlayer : MonoBehaviour
+{
+
+    private CriminalManager CriminalManager;
+    private HandcuffsManager HandcuffsManager;
+    [SerializeField] private HandcuffStack HandcuffStack;
+
+    private void Awake()
+    {
+        CriminalManager = this.GetComponent<CriminalManager>();
+        HandcuffsManager = this.GetComponent<HandcuffsManager>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Criminal") && !CriminalManager.CriminalLine.Contains(other.gameObject))
+        {
+            if (HandcuffsManager.GetNumberOfHandcuffs() > 0)
+            {
+                CaptureCriminal(other.gameObject);
+            }
+            else
+                Debug.Log("Out of Handcuffs");
+        }
+        else if(CriminalManager.CriminalLine.Contains(other.gameObject))
+            Debug.Log("Already captured this criminal");
+    }
+
+    private void CaptureCriminal(GameObject ob)
+    {
+        if (CriminalManager.CriminalLine.Count == 0)
+            ob.GetComponent<FollowTarget>().TargetTransform = this.transform;
+        else
+            ob.GetComponent<FollowTarget>().TargetTransform = CriminalManager.CriminalLine.Last.Value.transform;
+
+        CriminalManager.AddCriminalToCriminalLine(ob);
+        HandcuffsManager.RemoveHandcuff(1);
+        HandcuffStack.RemoveHandcuffToStack();
+
+    }
+
+}

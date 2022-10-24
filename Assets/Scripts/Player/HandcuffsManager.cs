831d8d2238b781f8b914ace304fd3c34dacbd65b
EfeTigli <netigli@hotmail.com>
2022-10-24 20:51:46 +0300

    Create HandcuffsManager Script

---
+++ Assets/Scripts/Player/HandcuffsManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandcuffsManager : MonoBehaviour
+{
+    [SerializeField] private int numberOfHandcuffs;
+
+    public void AddHandcuff(int value)
+    {
+        numberOfHandcuffs += value;
+    }
+
+    public int GetNumberOfHandcuffs()
+    {
+        return numberOfHandcuffs;
+    }
+}
